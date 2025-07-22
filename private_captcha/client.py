16202ddd6ae1d396e814454130d8de4dd961de57
Taras Kushnir <tk.dev@mailbox.org>
2025-07-22 11:54:18 +0300

    Fix pyproject.toml file

--- private_captcha/client.py
+++ private_captcha/client.py
@@ -23,7 +23,7 @@ log = logging.getLogger(__name__)
 GLOBAL_DOMAIN = "api.privatecaptcha.com"
 EU_DOMAIN = "api.eu.privatecaptcha.com"
 DEFAULT_FORM_FIELD = "private-captcha-solution"
-VERSION = "0.1"
+VERSION = "0.0.1"
 MIN_BACKOFF_MILLIS = 250
 RETRIABLE_STATUSES = {
     HTTPStatus.TOO_MANY_REQUESTS,
